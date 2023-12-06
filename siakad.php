63f0a8798cd928cdf017438204c67d0b75572997
Zain Sugeng <134490582+zainsugeng@users.noreply.github.com>
2023-12-06 21:00:26 +0700

    Add files via upload

---
+++ siakad.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Perpustakaan</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>SIAKAD student</h1>
+            <p>Universitas Binas Sarana Informatika</p>
+        </div>
+        <div class="main">
+            <div class="left">
+                <ul>
+                    <li>
+                        <div class="nama">Masukan NIM</div> 
+                        <div class="form">
+                            <input type="text">
+                        </div>
+                    </li>
+                    <li>
+                        <div class="nama">Password</div> 
+                        <div class="form">
+                            <input type="password">
+                        </div>
+                    </li>
+                    <li>
+                    <div class="nama">Jenjang Pendidikan</div> 
+                        <div class="form">
+                            <select name="jenjang">
+                                <option value="Sistem Informasi">D-III/S-1</option>
+                            </select>
+                        </div>
+                    </li>
+                    <li>
+                        <input type="submit" name="submit" value="LOGIN" class="btn">
+                    </li>
+                </ul>
+            </div>
+            <div class="right">
+                <h3>Selamat Datang</h3>
+                <p><img src="bsi.png"></p>
+                <p>Saat ini Anda berada di situs Sistem Informasi Akademik Mahasiswa (student.bsi.ac.id). Situs ini dikhususkan bagi mahasiswa/mahasiswi Akademi-akademi Bina Sarana Informatika dalam memperoleh informasi akademik (nilai), pembayaran biaya kuliah, pendaftaran ujian Her, TA, dll.</p>
+                <p>Gunakan Nomor Induk Mahasiswa (NIM) dan password yang berupa tanggal lahir Anda untuk mengakses Sisfo Akademik ini.</p>
+                <p>Terima kasih. Sukses selalu untuk Anda.</p>
+            </div>
+        </div>
+        <div class="footer">
+            <p align="center">Copyright &copy; 2023 Mushthofa Zain Sugeng Siakad</p>
+        </div>
+    </div>
+</body>
+</html>
